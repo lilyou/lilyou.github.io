60d863f99fa8e9d2927312a643ddec33243c64a6
lilyou <lilyouch@gmail.com>
2018-01-30 21:38:36 -0500

    scrolling jquery from mike

--- final-project/js/app.js
+++ final-project/js/app.js
@@ -15,6 +15,40 @@ $(function() {
   });
 });
 
+console.log($('#france'));
+
+$('#france').waypoint(function(event, direction){
+  console.log(direction, 'france')
+  
+  $('.country').html('<a href="album1.html">FRANCE</a>');
+
+  if (direction == 'up') {
+    console.log('bottom in view france');
+    offset: '50%'
+  }
+
+})
+
+$('#tajikistan').waypoint(function(event, direction){
+  console.log(direction, 'tajik')
+
+  if (direction == 'up') {
+    console.log('bottom in view tajik');
+    offset: '50%'  
+  }
+  
+  $('.country').html('<a href="album1.html">TAJIKISTAN</a>');
+})
+
+
+// var waypoint = new Waypoint({
+//   element: document.getElementById('france'),
+//   handler: function(direction) {
+//     console.log('Scrolled to waypoint!')
+//   }
+// })
+
+
 // Enlarge image click on index page and album page
 // Click to album page when click on country name
 // Change country name when scroll on index page 
